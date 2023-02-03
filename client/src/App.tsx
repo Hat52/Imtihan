96224f01af949834edd4fcfc3bed72ea28f94990
hamza ali <tanolihamzaali@gmail.com>
2023-02-04 02:23:05 +0500

    feat: Login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,47 @@
-import './App.css';
+import { Textfield, Button } from './components';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { MdOutlineEmail } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
 function App() {
 	return (
-		<div className="App">
-			<div className="Card"></div>
+		<div className="App w-screen h-screen flex justify-center items-center">
+			<div className="flex flex-col gap-5 justify-center items-center  h-full w-full ">
+				<h1 className="text-typography text-[32px] font-bold">Log in</h1>
+				<Textfield
+					Icon={MdOutlineEmail}
+					onChange={() => {
+						console.log('Change');
+					}}
+					error=""
+					placeholder="Email"
+					name="name"
+					type="email"
+					label="Enter your email"
+					value=""
+				/>
+				<Textfield
+					Icon={RiLockPasswordLine}
+					onChange={() => {
+						console.log('Change');
+					}}
+					error=""
+					placeholder="Password"
+					name="password"
+					type="password"
+					label="Enter your password"
+					value=""
+				/>
+				<Button text="Login" onClick={() => console.log('click')} />
+				<div className="flex w-[500px] justify-between">
+					<a href="/#" className="text-sm text-typography">
+						Forgot Password?
+					</a>
+					<a href="/#" className="text-sm text-typography">
+						Sign up?
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				secondary: '#1F37B5',
+				primary: '#121212',
+				'dark-grey': '#1E1E1E',
+				typography: '#E2E2E2',
+				error: '#FF0000'
+			}
+		}
 	},
 	plugins: []
 };
