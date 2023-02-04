6872296f9e59affe66c7be8698331a4e7b2118bb
hamza ali <tanolihamzaali@gmail.com>
2023-02-04 18:35:15 +0500

    change:full name field icon

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,13 +2,14 @@ import { Textfield, Button } from '../components';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import { MdOutlineEmail } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 export default function Signup() {
 	return (
 		<div className="flex flex-col gap-5 justify-center items-center  h-full w-full ">
 			<h1 className="text-typography text-[32px] font-bold">Log in</h1>
 			<Textfield
-				Icon={MdOutlineEmail}
+				Icon={BsFillPersonFill}
 				onChange={() => {
 					console.log('Change');
 				}}
