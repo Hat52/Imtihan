00a5c809c52a66f8d37561fc712a036a41be8269
hamza ali <tanolihamzaali@gmail.com>
2023-02-09 01:05:06 +0500

    Test page

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,3 +1,9 @@
+import { Card } from '../components';
+
 export default function Test() {
-	return <h1>Test</h1>;
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+			<Card />
+		</div>
+	);
 }
