484d45b8e8e1f4e6a1ed2039c2d82248a0c96648
hamza ali <tanolihamzaali@gmail.com>
2023-02-09 01:04:51 +0500

    add: Card component

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,23 @@
+export default function Card() {
+	return (
+		<div className="card flex flex-col items-center overflow-scroll hide-scrollbar gap-5 bg-[#1A1F1F] w-[350px] h-[500px] rounded-lg p-2 shadow-lg shadow-secondary">
+			<h4 className="text-typography text-center font-bold text-xl">Loops In programming</h4>
+			<p className="text-typography text-center">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+				has been the industry's standard dummy text ever since the 1500s, when an unknown
+				printer took a galley of type and scrambled it to make a type specimen book. It has
+				survived not only five centuries, but also the leap into electronic typesetting,
+				remaining essentially unchanged. It was popularised in the 1960s with the release of
+				Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+				publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum
+				is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+				industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book. It has survived not only
+				five centuries, but also the leap into electronic typesetting, remaining essentially
+				unchanged. It was popularised in the 1960s with the release of Letraset sheets
+				containing Lorem Ipsum passages, and more recently with desktop publishing software like
+				Aldus PageMaker including versions of Lorem Ipsum.
+			</p>
+		</div>
+	);
+}
