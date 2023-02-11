ca27c8fff36789e60838d38d0659b5de2e0d84cb
hamza ali <tanolihamzaali@gmail.com>
2023-02-12 01:54:18 +0500

    Animated card test implementation

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,9 +1,44 @@
+import { useState } from 'react';
 import { Card } from '../components';
 
 export default function Test() {
+	const [selected, setSelected] = useState(false);
+	const [move, setMove] = useState('start');
+
+	const setNextMove = (towards: string) => {
+		if (towards === 'previous') {
+			if (move === 'center') {
+				setMove('start');
+				return;
+			}
+			setMove('center');
+			return;
+		}
+		if (move === 'start') {
+			setMove('center');
+		} else if (move === 'center') {
+			setMove('end');
+		}
+	};
 	return (
-		<div className="w-screen h-screen flex items-center justify-center">
-			<Card />
+		<div className="w-screen h-screen flex overflow-hidden items-center justify-center">
+			<button
+				className="absolute left-0 top-0"
+				onClick={() => {
+					setSelected(!selected);
+					setNextMove('next');
+				}}>
+				Next
+			</button>
+			<button
+				className="absolute left-10 top-0"
+				onClick={() => {
+					setSelected(!selected);
+					setNextMove('previous');
+				}}>
+				Previous
+			</button>
+			<Card isSelected={selected} move={move} />
 		</div>
 	);
 }
