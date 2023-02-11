947efd464db4affe7dd4a12fe9011973af79587b
hamza ali <tanolihamzaali@gmail.com>
2023-02-12 02:39:59 +0500

    changes

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,6 +3,6 @@ export { default as Button } from './Button';
 export { default as Select } from './Select';
 export { default as Stepper } from './Stepper';
 export { default as Card } from './Card';
-export { default as ArrowSvg } from './ArrowSvg';
+export { default as ArrowSvg } from './ArrowSVG';
 
 // export { Textfield, Button, Select, Stepper };
