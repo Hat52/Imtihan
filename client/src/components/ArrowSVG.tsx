393e5a0bf22f5d02aa9d0406888a7fe447060005
hamza ali <tanolihamzaali@gmail.com>
2023-02-12 02:22:11 +0500

    update: moved arrow svg into a seperate component

---
+++ client/src/components/ArrowSVG.tsx
@@ -0,0 +1,26 @@
+interface IArrowSvg {
+	className: string;
+	handleClick: (event: React.MouseEvent<HTMLSpanElement>) => void;
+}
+
+const ArrowSvg = ({ className, handleClick }: IArrowSvg) => {
+	return (
+		<span onClick={handleClick}>
+			<svg
+				className={className}
+				aria-hidden="true"
+				fill="none"
+				stroke="#1F37B5"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg">
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M19 9l-7 7-7-7"></path>
+			</svg>
+		</span>
+	);
+};
+
+export default ArrowSvg;
